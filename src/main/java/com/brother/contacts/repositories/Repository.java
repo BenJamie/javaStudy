ba848d11f002ff8f23ca900a59aaf6a24aa2a00c
cybersoldier <zhanbin2008@gmail.com>
2017-01-15 10:53:55 -0500

    create a new Repository<E> class using EntityManager interface; replace the old AddressRepository and ContactRepository by a new one; changed: create(), update(), delete() methods.

--- src/main/java/com/brother/contacts/repositories/Repository.java
+++ src/main/java/com/brother/contacts/repositories/Repository.java
@@ -1,14 +1,28 @@
 package com.brother.contacts.repositories;
 
-import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 
-@Entity
 public class Repository<E> {
-    private final EntityManager em = Persistence.createEntityManagerFactory("EntityClass").createEntityManager();
-    public Repository<E> (){
-	
+
+    private final EntityManager em = Persistence.createEntityManagerFactory("training").createEntityManager();
+    private final Class<E> entityClass;
+
+    public Repository(Class<E> entityClass) {
+	super();
+	this.entityClass = entityClass;
+    }
+
+    public E find(long id) {
+	return em.find(entityClass, id);
+    }
+
+    public void save(E entity) {
+	em.merge(entity);
+    }
+
+    public void delete(E entity) {
+	em.remove(entity);
     }
 
 }
